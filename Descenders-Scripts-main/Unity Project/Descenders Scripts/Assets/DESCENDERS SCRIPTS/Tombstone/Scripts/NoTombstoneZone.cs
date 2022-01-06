4e085739aedb9a6062e5fb9b87c2e56940387027
nohumanman <nohumanman@gmail.com>
2022-01-06 19:28:22 +0000

    Initial Commit

---
+++ Descenders-Scripts-main/Unity Project/Descenders Scripts/Assets/DESCENDERS SCRIPTS/Tombstone/Scripts/NoTombstoneZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModTool.Interface;
+
+namespace TombstoneSystem{
+	public class NoTombstoneZone : ModBehaviour {
+		void OnTriggerEnter(Collider col) 
+		{
+			if (col.gameObject.GetComponent<TombstoneSystem.Tombstone>() != null){
+				Destroy(col.gameObject);
+			}
+		}
+	}
+}
