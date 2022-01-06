aef0dcdbd47df77d4e4ea21ff98ed24cd82b9a0c
nohumanman <nohumanman@gmail.com>
2022-01-06 19:29:06 +0000

    Initial Commit

---
+++ Descenders-Scripts-main/Unity Project/Descenders Scripts/Backup/DESCENDERS SCRIPTS/Landmarks/Scripts/LandmarkDescription.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModTool.Interface;
+using UnityEngine.UI;
+
+namespace Landmarks{
+	public class LandmarkDescription : ModBehaviour {
+		public Text description;
+		public Text title;
+		public Text PurchaseTitle;
+		public Button PurchaseButton;
+		public RawImage image;
+	}
+}
