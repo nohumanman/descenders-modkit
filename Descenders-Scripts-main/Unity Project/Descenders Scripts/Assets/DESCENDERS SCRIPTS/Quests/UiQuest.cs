62587033fa8c1b2b1152be15d3d8f5b59344913f
nohumanman <nohumanman@gmail.com>
2022-01-06 19:27:19 +0000

    Initial Commit

---
+++ Descenders-Scripts-main/Unity Project/Descenders Scripts/Assets/DESCENDERS SCRIPTS/Quests/UiQuest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ModTool.Interface;
+
+namespace Quests{
+	public class UiQuest : ModBehaviour {
+		public Text questNameAndDescription;
+		public GameObject Track;
+	}
+}
