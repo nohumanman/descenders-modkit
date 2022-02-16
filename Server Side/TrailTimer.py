7da4fb72e8152478b46e4e0ba16cc9f1341a82d0
nohumanman <nohumanman@gmail.com>
2022-02-16 13:14:28 +0000

    fixed discord integration

--- Server Side/TrailTimer.py
+++ Server Side/TrailTimer.py
@@ -3,6 +3,7 @@ from PlayerDB import PlayerDB
 import requests
 import random
 from Tokens import webhook
+import math
 
 
 class TimerNotStarted(Exception):
@@ -29,6 +30,18 @@ class TrailTimer():
         self.started = False
         self.times = []
 
+    def secs_to_str(self, secs):
+        d_mins = int(round(secs // 60))
+        d_secs = int(round(secs - (d_mins * 60)))
+        d_millis = int(round(secs-math.trunc(secs), 3) * 1000)
+        if len(str(d_mins)) == 1:
+            d_mins = "0" + str(d_mins)
+        if len(str(d_secs)) == 1:
+            d_secs = "0" + str(d_secs)
+        while len(str(d_millis)) < 3:
+            d_millis = str(d_millis) + "0"
+        return f"{d_mins}:{d_secs}.{d_millis}"
+
     def end_timer(self):
         self.started = False
         try:
@@ -40,30 +53,24 @@ class TrailTimer():
                 faster_amount = round(fastest_time - self.times[len(self.times)-1], 4)
                 emojis = ["🎉"]
                 data = {
-                    "content" : f"There's a new Time on {self.current_trail}!",
+                    "content" : f"There's a new Time on {self.player.trail}!",
                     "username" : "Descenders Gear Hub"
                 }
                 data["embeds"] = [
                     {
-                        "description" : f"🎉🎉 00:00:000 on {self.current_trail}!",
+                        "description" : f"🎉🎉 00:00:000 on {self.player.trail}!",
                         "title" : f"[Descenders Split Timer](https://gear-hub.nohumanman.com)",
                         "author" : {
-                            "name": f"{self.steam_name}",
+                            "name": f"{self.player.steam_name}",
                             "url": "",
-                            "icon_url": f"{self.avatar_src}"
+                            "icon_url": f"{self.player.avatar_src}"
                         },
                     }
                 ]
                         
-                content = ""
-                content += random.choice(emojis) * 3
-                content += "\n"
-                congrats = ["Congratulations to", "Congrats to", "GG", "Well raced", "Good job", "Well done"]
-                content += random.choice(congrats)
-                content += f" **{self.steam_name}** "
-                quickestest = ["fastest"]
-                content += f"for the new " + random.choice(quickestest) + f" time on {self.player.trail} in {self.current_world}!"
-                content += f"\nIt's {round(faster_amount, 5)} seconds faster than the previous best 🔥"
+                content = "🎉🎉🎉\n"
+                content += f"**{self.player.steam_name}** just got a new fastest time on {self.player.trail} in {self.player.world}!\n"
+                content += f"It's around {round(faster_amount, 5)} seconds faster than the previous best - that gives a time of **{self.secs_to_str(self.times[len(self.times)-1])}**! 🔥"
                 data = {
                     "content": content,
                     "username": "Descenders Competitive"
@@ -95,7 +102,7 @@ class TrailTimer():
                 float(client_time),
                 float(self.give)
             ) and anti_cheat:
-                self.times.append(client_time)
+                self.times.append(float(client_time))
             else:
                 raise AntiCheatMeasure("Client Time and Server Time are too far apart - cheating suspected!")
 
