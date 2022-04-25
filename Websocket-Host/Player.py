0421bbff37089519a9e24ce74296eb93bcab1a58
nohumanman <nohumanman@gmail.com>
2022-04-25 21:47:55 +0000

    Added Riders Gate

--- Websocket-Host/Player.py
+++ Websocket-Host/Player.py
@@ -1,3 +1,4 @@
+from http.client import GATEWAY_TIMEOUT
 import socket
 
 class NetPlayer():
@@ -6,6 +7,7 @@ class NetPlayer():
         self.steam_id = None
         self.steam_name = None
         self.world_name = None
+        self.gates = []
         self.send("PROBE")
 
     def send(self, data : str):
@@ -15,14 +17,30 @@ class NetPlayer():
         if data == "":
             return
         print(f"Handling data '{data}'")
+        data_list = data.split("|")
         if data.startswith("STEAM_ID"):
-            self.steam_id = data.split("|")[1]
+            self.steam_id = data_list[1]
         elif data.startswith("STEAM_NAME"):
-            self.steam_name = data.split("|")[1]
+            self.steam_name = data_list[1]
         elif data.startswith("WORLD_NAME"):
-            self.world_name = data.split("|")[1]
+            self.world_name = data_list[1]
         elif data.startswith("BOUNDRY_ENTER"):
-            self.on_boundry_enter()
+            self.on_boundry_enter(data_list[1], data_list[2])
+        elif data.startswith("BOUNDRY_EXIT"):
+            self.on_boundry_exit(data_list[1], data_list[2])
+        elif data.startswith("CHECKPOINT_ENTER"):
+            self.on_checkpoint_enter(data_list[1], data_list[2], data_list[3])
+        elif data.startswith("RESPAWN"):
+            self.on_respawn()
+        elif data.startswith("MAP_ENTER"):
+            self.on_map_enter(data_list[1], data_list[2])
+        elif data.startswith("MAP_EXIT"):
+            self.on_map_exit()
+        elif data.startswith("BIKE_SWITCH"):
+            self.on_bike_switch(data_list[1], data_list[2])
+        elif data.startswith("GATE_NAME"):
+            if (data_list[1] not in self.gates):
+                self.gates.append(data_list[1])
 
     def recieve(self):
         while True:
@@ -38,7 +56,7 @@ class NetPlayer():
     def on_respawn(self):
         pass
 
-    def on_bike_switch(self, old_bike, new_bike):
+    def on_bike_switch(self, old_bike : str, new_bike : str):
         pass
 
     def on_boundry_enter(self, boundry_guid, client_time):
