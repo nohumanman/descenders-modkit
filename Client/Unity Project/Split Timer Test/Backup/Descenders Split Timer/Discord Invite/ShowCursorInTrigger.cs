2cc117a71a355759131e996f24dcf3cf4d87173b
nohumanman <nohumanman@gmail.com>
2022-08-11 01:46:47 +0100

    Added discord invite feature

---
+++ Client/Unity Project/Split Timer Test/Backup/Descenders Split Timer/Discord Invite/ShowCursorInTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModTool.Interface;
+
+public class ShowCursorInTrigger : ModBehaviour {
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.transform.root.name == "Player_Human")
+            Cursor.visible = true;
+    }
+    void OnTriggerExit(Collider other){
+        if (other.gameObject.transform.root.name == "Player_Human")
+            Cursor.visible = false;
+    }
+}
