71d4421b97373e414aa5ef53890aca3021df0d09
nohumanman <nohumanman@gmail.com>
2022-08-13 23:56:31 +0100

    Added a seperate Unity Scene for Boundary Tool Maker

---
+++ Client/Unity Project NonModTool/Split Timer NonModTool/Assets/DELETE ON EXPORT/Boundary Tool/Scripts/BoundaryLinker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryLinker : MonoBehaviour {
+    public GameObject boundaryLinks;
+    public GameObject boundaries;
+    public GameObject boundary;
+    List<GameObject> boundaryPoints = new List<GameObject>();
+    List<GameObject> resentlySpawnedBoundaryPoints = new List<GameObject>();
+    public void SpawnBoundaries(){
+        boundaryPoints.Clear();
+        resentlySpawnedBoundaryPoints.Clear();
+        foreach(Transform child in boundaryLinks.transform)
+            boundaryPoints.Add(child.gameObject);
+        int i = 0;
+		foreach(GameObject boundaryPoint in boundaryPoints){
+            if (i+1 < boundaryPoints.Count)
+                SpawnBoundary(boundaryPoints[i], boundaryPoints[i+1]);
+            i++;
+        }
+    }
+    public void UndoBoundarySpawn(){
+        foreach(GameObject x in resentlySpawnedBoundaryPoints)
+            DestroyImmediate(x);
+    }
+    public void SpawnBoundary(GameObject from, GameObject to){
+        // Must be run on a unit cube.
+        Debug.Log("Creating boundary between " + from.name + " to " + to.name);
+        Vector3 center = from.transform.position - to.transform.position;
+        float distance = center.magnitude;
+        Vector3 posToSpawn = from.transform.position - (center / 2);
+        GameObject boundaryInstance = Instantiate(boundary);
+        boundaryInstance.transform.position = posToSpawn;
+        Vector3 localScale = boundaryInstance.transform.localScale;
+        boundaryInstance.transform.localScale = new Vector3(
+            localScale.x,
+            localScale.y,
+            distance
+        );
+        boundaryInstance.transform.LookAt(to.transform);
+        GameObject boundaryInstanceAtJoin = Instantiate(boundary);
+        boundaryInstanceAtJoin.transform.position = to.transform.position;
+        boundaryInstanceAtJoin.transform.localScale = new Vector3(5, 15, 5);
+        boundaryInstance.transform.SetParent(boundaries.transform);
+        boundaryInstanceAtJoin.transform.SetParent(boundaries.transform);
+        resentlySpawnedBoundaryPoints.Add(boundaryInstanceAtJoin);
+        resentlySpawnedBoundaryPoints.Add(boundaryInstance);
+    }
+}
---
+++ Client/Unity Project NonModTool/Split Timer NonModTool/Assets/DELETE ON EXPORT/Boundary Tool/Scripts/BoundaryLinkerEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(BoundaryLinker))]
+public class BoundaryLinkerEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        BoundaryLinker myScript = (BoundaryLinker)target;
+        if(GUILayout.Button("Spawn Boundaries"))
+            myScript.SpawnBoundaries();
+        if(GUILayout.Button("Undo Spawn"))
+            myScript.UndoBoundarySpawn();
+    }
+}
