b0f1336f686b590993484b21dbcde46e84a1ad2a
nohumanman <nohumanman@gmail.com>
2022-09-09 17:56:09 +0100

    when penalty is less than 0.1, no penalty is applied

--- Host/TrailTimer.py
+++ Host/TrailTimer.py
@@ -53,6 +53,8 @@ class TrailTimer():
                     self.current_penalty = (time.time()-self.exit_time)*10
                     if self.current_penalty < 2:
                         self.current_penalty = 2
+                    if self.current_penalty < 0.1:
+                        self.current_penalty = 0
                     self.total_running_penalty += self.current_penalty
                     self.network_player.send(
                         f"SPLIT_TIME|penalty of "
@@ -117,6 +119,7 @@ class TrailTimer():
                 )
             except Exception:
                 time_diff = 0
+            mess = ""
             if time_diff > 0:
                 mess = (
                     "<color=lime>-"
