b149075348f4ec2d35d0671b592ffc72c3a967bf
nohumanman <nohumanman@gmail.com>
2022-09-12 03:06:28 +0100

    added logic for gimbal cam based on existing cam

---
+++ Client/Mod Loader Solution/SplitTimer/GimbalCam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using ModLoaderSolution;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SplitTimer
+{
+    public class GimbalCam : MonoBehaviour
+    {
+        public GameObject ExistingCamera;
+        public bool ShouldLevel;
+        public void Start()
+        {
+            StartCoroutine(UpdateCamera());
+        }
+        public void Update()
+        {
+            if (ExistingCamera == null)
+                ExistingCamera = Camera.main.gameObject;
+            if (Input.GetKey(KeyCode.G) && Input.GetKeyDown(KeyCode.C))
+                ShouldLevel = !ShouldLevel;
+        }
+        public void LevelCamera()
+        {
+            if (ExistingCamera != null)
+                ExistingCamera.transform.eulerAngles = new Vector3(
+                    ExistingCamera.transform.eulerAngles.x,
+                    ExistingCamera.transform.eulerAngles.y,
+                    ExistingCamera.transform.eulerAngles.z * 0
+                );
+        }
+        public IEnumerator UpdateCamera()
+        {
+            while (true)
+            {
+                if (ShouldLevel)
+                    LevelCamera();
+                yield return null;
+            }
+        }
+    }
+}
