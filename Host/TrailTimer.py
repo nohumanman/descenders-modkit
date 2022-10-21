9b4ee322bb7a3766e28557e027d19d12c5eab295
nohumanman <nohumanman@gmail.com>
2022-10-21 18:01:00 +0100

    Update TrailTimer.py

--- Host/TrailTimer.py
+++ Host/TrailTimer.py
@@ -50,10 +50,10 @@ class TrailTimer():
         ):
             if self.exit_position is not None:
                 if (self.started):
-                    self.current_penalty = (time.time()-self.exit_time)*10
+                    self.current_penalty = (time.time()-self.exit_time)*100
                     if self.current_penalty < 2:
                         self.current_penalty = 2
-                    if self.current_penalty < 0.1:
+                    if self.current_penalty < 0.5:
                         self.current_penalty = 0
                     self.total_running_penalty += self.current_penalty
                     self.network_player.send(
