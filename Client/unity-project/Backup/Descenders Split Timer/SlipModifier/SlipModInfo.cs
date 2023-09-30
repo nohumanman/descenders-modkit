75300a6e608d8f0e43860555e124ffdf240f08fd
Matthew <nohumanman@gmail.com>
2023-09-30 23:00:00 +0100

    Removed files
    Added files
    
    Renamed files
    
    Add trees
    
    added stones
    
    added bikes
    
    Modified files
    
    added files
    
    added files
    
    added files
    
    added files
    
    added files

--- Client/unity-project/Backup/Descenders Split Timer/SlipModifier/SlipModInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using ModTool.Interface;
-
-public class SlipModInfo : ModBehaviour {
-    public float slipAim = 2f;
-    public float skidSlipAim = 0.2f;
-}
--- Client/unity-project/Backup/Descenders Split Timer/Split Timer/Scripts/JsonRidersGate.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using ModTool.Interface;
-public class JsonRidersGate : ModBehaviour {
-    public AudioClip beforeOpen;
-    public AudioClip onOpen;
-}
