26593f8740459b6e3f718fe7eaa3d21c491e1c13
Matthew <nohumanman@gmail.com>
2023-11-05 17:37:45 +0000

    Remove getplayer in favour of using subject variable

--- mod-loader-solution/FollowCamSystem.cs
+++ mod-loader-solution/FollowCamSystem.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +9,8 @@ namespace ModLoaderSolution
 {
     public class FollowCamSystem : MonoBehaviour
     {
-        public List<Vector3> camLocations = new List<Vector3>() { new Vector3(5, 210, 525), new Vector3(41, 197, 507), new Vector3(57, 201, 511) };
+        public GameObject subject;
+        public List<Vector3> camLocations = new List<Vector3>() { new Vector3(5, 210, 525), new Vector3(41, 197, 507), new Vector3(57, 201, 511), new Vector3(109, 193, 508) };
         public Vector3 currentCamLoc = Vector3.zero;
         public void Update()
         {
@@ -18,7 +19,7 @@ namespace ModLoaderSolution
             bool anyValid = false;
             foreach (Vector3 vector3 in camLocations)
                 if (IsValid(vector3)){ // if camera is valid
-                    float distanceToCam = Vector3.Distance(vector3, Utilities.instance.GetPlayer().transform.position);
+                    float distanceToCam = Vector3.Distance(vector3, subject.transform.position);
                     // if this is the closest to our player
                     if (distanceToCam < closest)
                     {
@@ -28,7 +29,7 @@ namespace ModLoaderSolution
                     }
                 }
             if (!anyValid)
-                currentCamLoc = Utilities.instance.GetPlayer().transform.position - Utilities.instance.GetPlayer().transform.forward*3;
+                currentCamLoc = subject.transform.position - subject.transform.forward*3;
             if (currentCamLoc != Vector3.zero)
             {
                 foreach(Camera cam in FindObjectsOfType<Camera>())
@@ -36,21 +37,21 @@ namespace ModLoaderSolution
                     // set position of camera to current camera location
                     cam.transform.position = currentCamLoc;
                     // temporarily just set the rotation to look at player
-                    cam.transform.LookAt(Utilities.instance.GetPlayer().transform);
+                    cam.transform.LookAt(subject.transform);
                 }
             }
         }
         public bool IsValid(Vector3 camPos, int proximity = 5000)
         {
-            if (Utilities.instance.GetPlayer() == null)
+            if (subject == null)
                 return false;
-            if (Vector3.Distance(Utilities.instance.GetPlayer().transform.position, camPos) > proximity)
+            if (Vector3.Distance(subject.transform.position, camPos) > proximity)
                 return false;
             // if player is in line of sight of camera
-            Vector3 directionToPlayer = Utilities.instance.GetPlayer().transform.position - camPos;
+            Vector3 directionToPlayer = subject.transform.position - camPos;
             RaycastHit hit;
             if (Physics.Raycast(camPos, directionToPlayer, out hit, proximity))
-                if (hit.collider.gameObject.transform.root.gameObject == Utilities.instance.GetPlayer())  // root of collider is player_human
+                if (hit.collider.gameObject.transform.root.gameObject == subject)  // root of collider is player_human
                     return true; // player is in line of view
             return false;
         }
