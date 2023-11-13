318474ce4f327698682f8aa8f0d378beac813a51
Matthew <nohumanman@gmail.com>
2023-11-13 16:48:05 +0000

    Add keybind to disable time modifier

--- mod-loader-solution/Modifiers/TimeModifier.cs
+++ mod-loader-solution/Modifiers/TimeModifier.cs
@@ -9,6 +9,7 @@ namespace ModLoaderSolution
     {
         public static TimeModifier Instance { get; private set; }
         public float speed = 1f;
+        public bool enabled = true;
         void Awake()
         {
             if (Instance != null && Instance != this)
@@ -18,6 +19,14 @@ namespace ModLoaderSolution
         }
         void Update()
         {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Y))
+                enabled = !enabled;
+            if (!enabled)
+            {
+                speed = 1f;
+                return;
+            }
+
             if (Input.GetKeyDown("joystick button 8"))
                 speed = 0.5f;
             if (Input.GetKeyUp("joystick button 8"))
