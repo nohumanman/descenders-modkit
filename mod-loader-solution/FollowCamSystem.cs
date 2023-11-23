8a6dc2886fb296ddf5e3bbdd5331313171793b77
Matthew <nohumanman@gmail.com>
2023-11-23 22:45:25 +0000

    Add load and save functions

--- mod-loader-solution/FollowCamSystem.cs
+++ mod-loader-solution/FollowCamSystem.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,6 +14,75 @@ namespace ModLoaderSolution
         public GameObject subject;
         public List<Vector3> camLocations = new List<Vector3>() { };
         public Vector3 currentCamLoc = Vector3.zero;
+        public void LoadFromFile()
+        {
+            // credit ChatGPT
+            string filePath = Path.Combine(Application.persistentDataPath, "CamLocations.txt");
+
+            // Check if the file exists before attempting to read from it
+            if (File.Exists(filePath))
+            {
+                camLocations.Clear(); // Clear existing camLocations data before loading
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] values = line.Split(',');
+                        if (values.Length == 3)
+                        {
+                            float x, y, z;
+                            if (float.TryParse(values[0], out x) && float.TryParse(values[1], out y) && float.TryParse(values[2], out z))
+                            {
+                                Vector3 loadedVector = new Vector3(x, y, z);
+                                camLocations.Add(loadedVector);
+                            }
+                            else
+                            {
+                                Debug.LogWarning("Failed to parse Vector3 from file.");
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogWarning("Invalid line format in file.");
+                        }
+                    }
+                }
+
+                Debug.Log("CamLocations loaded from file.");
+            }
+            else
+            {
+                Debug.LogWarning("File does not exist or couldn't be found.");
+            }
+        }
+
+        public void SaveToFile()
+        {
+            // credit ChatGPT
+            string filePath = Path.Combine(Application.persistentDataPath, "CamLocations.txt");
+
+            // Ensure camLocations has data to save
+            if (camLocations.Count > 0)
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (Vector3 location in camLocations)
+                    {
+                        string line = $"{location.x},{location.y},{location.z}";
+                        writer.WriteLine(line);
+                    }
+                }
+
+                Debug.Log("CamLocations saved to file.");
+            }
+            else
+            {
+                Debug.LogWarning("No camLocations data to save.");
+            }
+        }
+
         public void OnGUI()
         {
             int pos = 0;
@@ -23,24 +92,30 @@ namespace ModLoaderSolution
                 string x = GUI.TextField(new Rect(20, 20+pos, 60, 20), vector3.x.ToString());
                 string y = GUI.TextField(new Rect(20+60, 20 + pos, 60, 20), vector3.y.ToString());
                 string z = GUI.TextField(new Rect(20+60+60, 20 + pos, 60, 20), vector3.z.ToString());
-                string fov = GUI.TextField(new Rect(20 + 60 + 60+60, 20 + pos, 60, 20), Camera.main.fieldOfView.ToString());
-                if (GUI.Button(new Rect(20 + 60 + 60 + 60 + 60, 20 + pos, 60, 20), "SET"))
+                if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 60, 20), "SET"))
                     camLocations[i] = Camera.main.transform.position;
                 else
                     camLocations[i] = new Vector3(float.Parse(x), float.Parse(y), float.Parse(z));
-                if (GUI.Button(new Rect(20 + 60 + 60 + 60 + 60 + 60, 20 + pos, 60, 20), "GOTO"))
+                if (GUI.Button(new Rect(20 + 60 + 60 + 60 + 60, 20 + pos, 60, 20), "GOTO"))
                     Camera.main.transform.position = vector3;
-                if (GUI.Button(new Rect(20 + 60 + 60 + 60 + 60 + 60 + 60, 20 + pos, 60, 20), "DEL"))
+                if (GUI.Button(new Rect(20 + 60 + 60 + 60 + 60 + 60, 20 + pos, 60, 20), "DEL"))
                     camLocations.RemoveAt(i);
                 pos += 22;
                 i++;
             }
             if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 240, 20), "Toggle Follow Cam"))
                 bother = !bother;
+            pos += 22;
             if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 240, 20), "Look at me"))
                 Camera.main.transform.LookAt(Utilities.instance.GetPlayer().transform);
+            pos += 22;
             if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 240, 20), "Add Camera"))
                 camLocations.Add(Vector3.zero);
+            pos += 22;
+            if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 240, 20), "Save to CamLocations.txt"))
+                SaveToFile();
+            if (GUI.Button(new Rect(20 + 60 + 60 + 60, 20 + pos, 240, 20), "Load from CamLocations.txt"))
+                LoadFromFile();
             GUIStyle myButtonStyle2 = new GUIStyle(GUI.skin.button);
             myButtonStyle2.normal.textColor = Color.white;
             myButtonStyle2.normal.background = UserInterface.MakeTex(5, 5, new Color(0.2f, 0.06f, 0.12f));
