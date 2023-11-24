f79687522f5e131f2687bb672c25a9effc5e2977
Matthew <nohumanman@gmail.com>
2023-11-24 15:49:45 +0000

    Add zoomamount

--- mod-loader-solution/FollowCamSystem.cs
+++ mod-loader-solution/FollowCamSystem.cs
@@ -12,9 +12,10 @@ namespace ModLoaderSolution
     public class Cam
     {
         public Vector3 loc = Vector3.zero; // the location of the camera
-        public float panSpeed = 20; // the speed the camera follows the player rotationally
-        public float proximity; // the range a camera can see
-        public bool shouldZoom; // whether the camera should zoom to correct FOV
+        public float panSpeed = 13; // the speed the camera follows the player rotationally
+        public float proximity = 100; // the range a camera can see
+        public float zoomAmount = 5f; // the amount the camera should be zoomed to the player
+        public bool shouldZoom = true; // whether the camera should zoom to correct FOV
     }
     public class FollowCamSystem : MonoBehaviour
     {
@@ -25,13 +26,13 @@ namespace ModLoaderSolution
         private bool bother = true;
         public void LoadFromFile()
         {
-            // credit ChatGPT
             string filePath = Path.Combine(Application.persistentDataPath, "cameras.txt");
 
-            // Check if the file exists before attempting to read from it
+            // Check if the file exists
             if (File.Exists(filePath))
             {
-                cameras.Clear(); // Clear existing cameras data before loading
+                // Clear existing cameras data
+                cameras.Clear();
 
                 using (StreamReader reader = new StreamReader(filePath))
                 {
@@ -39,32 +40,41 @@ namespace ModLoaderSolution
                     while ((line = reader.ReadLine()) != null)
                     {
                         string[] values = line.Split(',');
-                        if (values.Length == 3)
+
+                        if (values.Length == 6)
                         {
-                            float x, y, z;
-                            if (float.TryParse(values[0], out x) && float.TryParse(values[1], out y) && float.TryParse(values[2], out z))
+                            float x, y, z, proximity, panSpeed;
+                            bool shouldZoom;
+
+                            if (float.TryParse(values[0], out x) && float.TryParse(values[1], out y) && float.TryParse(values[2], out z)
+                                && float.TryParse(values[3], out proximity) && float.TryParse(values[4], out panSpeed)
+                                && bool.TryParse(values[5], out shouldZoom))
                             {
+                                // Create a new Cam object and add it to the cameras list
                                 Cam cam = new Cam();
                                 cam.loc = new Vector3(x, y, z);
+                                cam.proximity = proximity;
+                                cam.panSpeed = panSpeed;
+                                cam.shouldZoom = shouldZoom;
                                 cameras.Add(cam);
                             }
                             else
                             {
-                                Debug.LogWarning("Failed to parse Vector3 from file.");
+                                Debug.LogWarning("Error parsing data from file. Skipping line: " + line);
                             }
                         }
                         else
                         {
-                            Debug.LogWarning("Invalid line format in file.");
+                            Debug.LogWarning("Invalid data format in file. Skipping line: " + line);
                         }
                     }
                 }
 
-                Debug.Log("cameras loaded from file.");
+                Debug.Log("Cameras loaded from file.");
             }
             else
             {
-                Debug.LogWarning("File does not exist or couldn't be found.");
+                Debug.LogWarning("File not found: " + filePath);
             }
         }
 
@@ -80,7 +90,7 @@ namespace ModLoaderSolution
                 {
                     foreach (Cam cam in cameras)
                     {
-                        string line = $"{cam.loc.x},{cam.loc.y},{cam.loc.z},{cam.proximity},{cam.panSpeed},{cam.shouldZoom}";
+                        string line = $"{cam.loc.x},{cam.loc.y},{cam.loc.z},{cam.proximity},{cam.panSpeed},{cam.shouldZoom},{cam.zoomAmount}";
                         writer.WriteLine(line);
                     }
                 }
@@ -96,7 +106,7 @@ namespace ModLoaderSolution
         {
             int yPos = 20;
             int i = 0;
-            List<string> title = new List<string>() { "X", "Y", "Z", "PanSpeed", "ShouldZoom", "Proximity"};
+            List<string> title = new List<string>() { "X", "Y", "Z", "PanSpeed", "ShouldZoom", "Proximity", "zoomAmount"};
             float xPos = 20;
             foreach (string key in title)
             {
@@ -114,7 +124,8 @@ namespace ModLoaderSolution
                 cam.panSpeed = float.Parse(GUI.TextField(new Rect(xPos, yPos, 60, 20), cam.panSpeed.ToString())); xPos += 65;
                 cam.shouldZoom = GUI.Toggle(new Rect(xPos, yPos, 60, 20), cam.shouldZoom, "shouldZoom"); xPos += 65;
                 cam.proximity = float.Parse(GUI.TextField(new Rect(xPos, yPos, 60, 20), cam.proximity.ToString())); xPos += 65;
-
+                cam.zoomAmount = float.Parse(GUI.TextField(new Rect(xPos, yPos, 60, 20), cam.zoomAmount.ToString())); xPos += 65;
+                
                 if (GUI.Button(new Rect(xPos, yPos, 60, 20), "SET"))
                     cameras[i].loc = Camera.main.transform.position;
                 xPos += 60;
@@ -149,8 +160,9 @@ namespace ModLoaderSolution
         }
         public Cam GetBestCamera()
         {
+            return cameras[0];
             float closest = Mathf.Infinity;
-            Cam closestCam = new Cam();
+            Cam closestCam = cameras[0];
             float switchThreshold = 2f;
             foreach (Cam cam in cameras)
             {
@@ -158,7 +170,6 @@ namespace ModLoaderSolution
                 if (IsValid(vector3))
                 {
                     float distanceToCam = Vector3.Distance(vector3, subject.transform.position);
-                    
                     if (distanceToCam < closest - switchThreshold) // if this is the closest to our player
                     {
                         closest = distanceToCam; // set as closest
@@ -183,20 +194,18 @@ namespace ModLoaderSolution
 
             Cam bestCam = GetBestCamera();
             Vector3 bestLoc = bestCam.loc;
+            if (currentCam == null)
+                currentCam = bestCam;
             if (bestLoc != currentCam.loc && bestLoc != Vector3.zero)
             {
-                currentCam.loc = bestLoc;
+                currentCam = bestCam;
                 shouldSnap = true;
             }
-
             if (currentCam.loc != Vector3.zero)
             {
                 foreach(Camera cam in FindObjectsOfType<Camera>())
                 {
-                    // set position of camera to current camera location
-                    cam.transform.position = currentCam.loc;
-                    // temporarily just set the rotation to look at player
-                    //cam.transform.LookAt(subject.transform);
+                    cam.transform.position = currentCam.loc; // set position of camera to current camera location
                     if (shouldSnap)
                     {
                         cam.transform.LookAt(subject.transform.position);
@@ -208,12 +217,12 @@ namespace ModLoaderSolution
                     }
 
                     float distanceToSubject = Vector3.Distance(cam.transform.position, subject.transform.position);
-                    const float someConstant = 5f;
+                    float someConstant = currentCam.zoomAmount;
                     // Calculate FOV based on distance (adjust this formula based on your needs)
                     float desiredFOV = Mathf.Atan(someConstant / distanceToSubject) * Mathf.Rad2Deg * 2f;
 
                     // Set the FOV within some defined range (e.g., between 20 and 100)
-                    cam.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, desiredFOV, 13 * Time.deltaTime);//Mathf.Clamp(desiredFOV, 20, 100);
+                    cam.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, desiredFOV, currentCam.panSpeed * Time.deltaTime);//Mathf.Clamp(desiredFOV, 20, 100);
 
 
                 }
