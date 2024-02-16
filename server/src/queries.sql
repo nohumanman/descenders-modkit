77edd0186374326f8c5f900413f9824ccfcd1eba
Matthew Bayley <matthew.bayley@student.reading.ac.uk>
2024-02-16 22:24:01 +0000

    Add get_average_start_time method to DBMS class

--- server/src/queries.sql
+++ server/src/queries.sql
@@ -287,3 +287,13 @@ GROUP BY
     Player.steam_id
 ORDER BY
     MIN(SplitTime.checkpoint_time) ASC;
+
+
+
+
+-- name: get_average_start_time^
+-- Get the average start time for a given trail
+SELECT AVG(starting_speed) AS value
+FROM Time
+WHERE trail_name = :trail_name
+and ignored = 0 and verified = 1;
