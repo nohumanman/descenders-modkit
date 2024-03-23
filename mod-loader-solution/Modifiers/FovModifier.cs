67967421429c4aa3a26656c42a8f897a57323624
Matthew Bayley <nohumanman@gmail.com>
2024-03-23 10:32:21 +0000

    Add FovModifier class for modifying field of view

---
+++ mod-loader-solution/Modifiers/FovModifier.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ModLoaderSolution;
+using System;
+
+namespace ModLoaderSolution
+{
+	public class FovModifier : MonoBehaviour
+	{
+		public float targetFov = -1;
+		public float fovOffset = 0;
+
+		public void Update()
+		{
+			foreach (Camera x in FindObjectsOfType<Camera>())
+			{
+				x.fieldOfView += fovOffset;
+			}
+		}
+	}
+}
