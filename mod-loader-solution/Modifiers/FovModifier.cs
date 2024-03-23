83a81548282704aa43d54752470c241cbed9dc78
Matthew Bayley <nohumanman@gmail.com>
2024-03-23 10:36:52 +0000

    Add FovModifier singleton instance

--- mod-loader-solution/Modifiers/FovModifier.cs
+++ mod-loader-solution/Modifiers/FovModifier.cs
@@ -9,9 +9,16 @@ namespace ModLoaderSolution
 {
 	public class FovModifier : MonoBehaviour
 	{
+		public static FovModifier Instance { get; private set; }
 		public float targetFov = -1;
 		public float fovOffset = 0;
-
+		void Awake()
+		{
+			if (Instance != null && Instance != this)
+				Destroy(this);
+			else
+				Instance = this;
+		}
 		public void Update()
 		{
 			foreach (Camera x in FindObjectsOfType<Camera>())
